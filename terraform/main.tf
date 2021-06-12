42aba97f1ffb0b4247ad03385e29a5c287b1e942
rashmi-kanekar <48589934+rashmi-kanekar@users.noreply.github.com>
2021-06-12 19:03:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "=2.63.0"
     }
   }
-
    backend "azurerm" {
     resource_group_name  = "TerraformRG"
     storage_account_name = "strg788"
@@ -19,8 +18,7 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example"
   location = "West Europe"
 }
