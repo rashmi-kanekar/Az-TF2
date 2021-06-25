13e3a505cbc203e0d6883ccf283c7dcfd5d08d55
rashmi-kanekar <48589934+rashmi-kanekar@users.noreply.github.com>
2021-06-25 23:40:27 +0530

    Update main.tf

--- terraform/RG/main.tf
+++ terraform/RG/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformRG"
+    storage_account_name = "strg788"
+    container_name       = "terraform"
+    #key                  = "${var.RG}/terraform.tfstate"
+    key                  = ${var.RG}"/terraform.tfstate"
+  }
+}
+
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
