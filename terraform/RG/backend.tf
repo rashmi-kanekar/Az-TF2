8cd378901f20a1e8edeffcc3a880f144623fc13e
rashmi-kanekar <48589934+rashmi-kanekar@users.noreply.github.com>
2021-06-25 23:38:54 +0530

    Delete backend.tf

--- terraform/RG/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "TerraformRG"
-    storage_account_name = "strg788"
-    container_name       = "terraform"
-    key                  = "${var.RG}/terraform.tfstate"
-  }
-}
